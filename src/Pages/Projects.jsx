a943c3bfd131ba4550d71a83d73a6e589f55cc03
adityaimrd <adtyimrd27@gmail.com>
2025-07-05 23:09:40 +0700

    revisi3

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -13,7 +13,7 @@ const Projects = () => {
       id: 1,
       imageSrc: porto1Image, 
       title: 'Cerita Kita',
-      description: 'Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      description: 'Cerita Kita is a project designed as a platform for storing personal stories, where each narrative can be enriched with visual and geographical elements such as maps, photos, and of course, the story itself.',
       techStack: ['HTML', 'CSS', 'JavaScript', 'Tailwind'],
       liveLink: 'https://ceritakitaa.netlify.app/' 
     },
@@ -21,7 +21,7 @@ const Projects = () => {
       id: 2,
       imageSrc: porto2Image,
       title: 'APP Story',
-      description: 'Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      description: 'APP Story is a project designed as a platform for storing personal stories, where each narrative can be enriched with visual and geographical elements such as maps, photos, and of course, the story itself.',
       techStack: ['HTML', 'CSS', 'JavaScript'],
       liveLink: 'https://storypa.netlify.app/' 
     },
@@ -29,7 +29,7 @@ const Projects = () => {
       id: 3,
       imageSrc: porto3Image,
       title: 'Eduwise',
-      description: 'Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      description: 'EduWise is a Progressive Web App (PWA) developed to help individuals identify their learning style (Visual, Auditory, Reading, or Kinesthetic) and get recommendations for appropriate learning methods.',
       techStack: ['HTML', 'CSS', 'JavaScript', 'Vue', 'Python'],
       liveLink: 'https://eduwise-ten.vercel.app/' 
     }
